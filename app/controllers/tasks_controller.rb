8452ffac210b289a4324f6267f2edff307a6d4f6
y129may9th <y129may9th@gmail.com>
2019-03-21 19:07:46 +0900

    Add edit, delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -10,6 +12,13 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "Updated [#{task.name}] 😀"
   end
 
   def create
@@ -17,6 +26,13 @@ class TasksController < ApplicationController
     task.save!
     redirect_to tasks_url, notice: "task [#{task.name}] was saved!"
   end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "Deleted [#{task.name}] 😈"
+  end
+
 
   private 
 
