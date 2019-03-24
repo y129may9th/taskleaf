a303a9ad0aa9f129af9a1de190c9533f1a1b6e65
yui <44105111+y129may9th@users.noreply.github.com>
2019-03-24 19:25:22 +0900

    Revert "Add Login/Logout"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :users
-  end
-
-  root to: 'tasks#index'
-  resources :tasks
+    root to: 'tasks#index'
+    resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
