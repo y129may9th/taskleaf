0f579945aa7384bc141ae3e7fb69f55dab90e296
y129may9th <y129may9th@gmail.com>
2019-04-05 21:06:06 +0900

    Add Association

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.recent
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,28 +13,25 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "Updated [#{task.name}] 😀"
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "Updated [#{@task.name}] 😀"
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     if @task.save
-        redirect_to @task, notice: "task [#{task.name}] was saved!👏"
+        redirect_to @task, notice: "task [#{@task.name}] was saved!👏"
     else
         render :new
     end
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "Deleted [#{task.name}] 😈"
+    @task.destroy
+    redirect_to tasks_url, notice: "Deleted [#{@task.name}] 😈"
   end
 
 
@@ -41,6 +39,10 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
 
