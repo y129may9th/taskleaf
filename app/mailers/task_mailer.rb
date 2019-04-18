c9a34ac1f06453a9ca84c898dc7ca71001febb64
y129may9th <y129may9th@gmail.com>
2019-04-18 19:17:17 +0900

    Setting Mailcatcher

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -4,8 +4,8 @@ class TaskMailer < ApplicationMailer
   def creation_email(task)
     @task = task
     mail(
-    subject: 'タスク完了メール'
-    to: 'user@example.com'
+    subject: 'タスク完了メール', 
+    to: 'user@example.com', 
     from: 'taskleaf@example.com'
         )
   end
