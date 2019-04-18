9ea13889982a96b0a0a8e853d2b11605154bf031
y129may9th <y129may9th@gmail.com>
2019-04-18 20:02:46 +0900

    Add Spec/mailers

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -4,7 +4,7 @@ class TaskMailer < ApplicationMailer
   def creation_email(task)
     @task = task
     mail(
-    subject: 'タスク完了メール', 
+    subject: 'タスク作成完了メール', 
     to: 'user@example.com', 
     from: 'taskleaf@example.com'
         )
