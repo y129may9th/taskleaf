a68b0bad1378cff0207575d3e9239f3406955ce0
y129may9th <y129may9th@gmail.com>
2019-04-18 22:37:07 +0900

    Add CSVFile inport (Model)

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,12 +15,6 @@ class Task < ApplicationRecord
     []
   end
 
-  private 
-
-  def validate_name_not_include_comma
-    errors.add(:name, ' : Do not include comma in "name form"') if name&.include?(',')
-  end
-
   def self.csv_attributes
     ["name", "description", "created_at", "updated_at"]
   end
@@ -32,6 +26,20 @@ class Task < ApplicationRecord
         csv << csv_attributes.map{ |attr| task.send(attr) }
       end
     end
+  end
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      task = new
+      task.attributes = row.to_hash.slice(*csv_attributes)
+      task.save!
+    end
+  end
+
+  private
+
+  def validate_name_not_include_comma
+    errors.add(:name, ' : Do not include comma in "name form"') if name&.include?(',')
   end
 end
 
